d83ffe0f94d45e3f2a2f600137c9344faef8ad29
Renzhentaxi <renzhentaxibaerde@gmail.com>
2016-12-07 17:14:24 -0500

    finished Excel.py still needs comments and added a new file Final.py that links the codes together

--- Excel.py
+++ Excel.py
@@ -1,7 +1,7 @@
 import openpyxl
-
-def get_workbook(date):
-    file_name = date.isoformat()
+from openpyxl.chart import ScatterChart, Reference, Series
+import os
+def get_workbook(file_name):
     if os.path.exists(file_name):
         return openpyxl.load_workbook(file_name)
     else:
@@ -12,4 +12,22 @@ def get_workbook(date):
         return workbook
 
 def enter_data(time, temp, worksheet):
-def update_chart(workbook, worksheet)
+    max_row = worksheet.max_row
+    if worksheet.cell(row = max_row, column = 1).value != time: #only enter data if its different from last entr
+        worksheet.cell(row = max_row + 1, column = 1).value = time
+        worksheet.cell(row = max_row + 1, column = 2).value = temp
+
+def update_chart(date, worksheet):
+    chart = ScatterChart()
+    chart.title = date.isoformat()
+    chart.x_axis.title = worksheet["A1"].value
+    chart.y_axis.title = worksheet["B1"].value
+    chart.legend = None
+
+    max_row = worksheet.max_row
+    x_values = Reference(worksheet, min_col = 1, min_row = 2, max_col = 1, max_row = max_row)
+    y_values = Reference(worksheet, min_col = 2, min_row = 2, max_col = 2, max_row = max_row)
+    series = Series(y_values, x_values)
+    chart.series.append(series)
+
+    worksheet.add_chart(chart, "F1")
---
+++ Final.py
@@ -0,0 +1,19 @@
+import Excel
+import Mail
+import XML
+
+
+
+xml = XML.get_xml() #get weather report in xml format
+date, time, temp = XML.parse_xml(xml) #parse and extract xml into date, time, temp
+
+file_name = date.isoformat() + ".xlsx" #file name = date.xlsx
+
+#get workbook and worksheet
+workbook = Excel.get_workbook(file_name)
+worksheet = workbook.active
+
+Excel.enter_data(time, temp, worksheet) #enter time, temp data into the sheet
+Excel.update_chart(date, worksheet) #update the chart on the sheet
+
+workbook.save(file_name) #save workbook
