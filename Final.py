9c0e37db4c2b2eac521be7455ea714ffb2f6ee7a
Renzhentaxi <renzhentaxibaerde@gmail.com>
2016-12-07 15:19:20 -0500

    get_report() method

--- Final.py
+++ Final.py
@@ -1,1 +1,17 @@
 import requests
+
+
+def get_report():
+    url = "http://www.aviationweather.gov/adds/dataserver_current/httpparam"
+    parameters = {
+    'dataSource' : 'metars',
+    'requestType' : 'retrieve',
+    'format': 'xml',
+    'stationString' : 'kjfk',
+    'mostRecent' : True,
+    'hoursBeforeNow': 1
+    }
+    response = requests.get(url, params = parameters)
+    return response.text
+
+print(get_report())
