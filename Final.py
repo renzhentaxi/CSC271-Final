9455c2adc9f66baad84138827b3e16ff7c047064
Renzhentaxi <renzhentaxibaerde@gmail.com>
2016-12-07 15:37:07 -0500

    added parse_report method and comments for get_report

--- Final.py
+++ Final.py
@@ -1,6 +1,7 @@
 import requests
-
-
+import xml.etree.ElementTree as ET
+import datetime
+#: returns a string representation of the report in xml
 def get_report():
     url = "http://www.aviationweather.gov/adds/dataserver_current/httpparam"
     parameters = {
@@ -14,4 +15,19 @@ def get_report():
     response = requests.get(url, params = parameters)
     return response.text
 
-print(get_report())
+def parse_report(report):
+    element = ET.fromstring(report).find('data').find('METAR')
+
+    date, time = element.find('observation_time').text[:-1].split('T', 1) 
+    year, month, day = [int(i) for i in date.split("-")]
+    hour, minute, second = [int(i) for i in time.split(":")]
+
+    data = {
+    'temp': float(element.find('temp_c').text),
+    'day' : datetime.date(year, month, day),
+    'time': datetime.time(hour, minute, second)
+    }
+
+    return data
+
+print(parse_report(get_report()))
